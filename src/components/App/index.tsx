087f0fa3d5c1cd6141fc726132765c899de36efd
pumanitro <pumanitro@gmail.com>
2022-06-16 21:47:09 +0200

    Handled map initiation with RND configuration.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,8 +3,12 @@ import React, {useEffect, useState} from "react"
 import { HexGrid, Layout, Hexagon, Text, GridGenerator } from "react-hexgrid";
 import {RNGService, Tile} from "../../services/RNGService";
 
+const getTileKey = (x: number, y: number, z: number) => {
+  return `_${x},${y},${z}`;
+}
+
 export const App: React.FC = () => {
-  const [_map, setMap] = useState<Tile[]>([]);
+  const [map, setMap] = useState<Tile[]>([]);
 
   const hexagons = GridGenerator.hexagon(1);
 
@@ -16,9 +20,22 @@ export const App: React.FC = () => {
     })()
   }, []);
 
-  return <HexGrid width={1200} height={1000} spacing={1.1} >
+  const indexedMap = map.reduce((acc: Record<string, Tile>, tile) => {
+    const key = getTileKey(tile.x, tile.y, tile.z);
+    acc[key] = tile;
+    return acc;
+  } ,{})
+
+  return <HexGrid width={1200} height={1000}>
     <Layout size={{ x: 6, y: 6 }}>
-      { hexagons.map((hex: any, i: number) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+      { hexagons.map((hex: any, i: number) => {
+        const mapTile = indexedMap[getTileKey(hex.q, hex. r, hex.s)];
+
+        return <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}>{
+            mapTile &&
+            <Text>{mapTile.value}</Text>
+        }</Hexagon>
+      }) }
     </Layout>
   </HexGrid>;
 }
